3edee95ca0adc599d961cb69f81e956cbe0c5c38
Alexey Matyunin <thcoker_m@mail.ru>
2024-03-14 17:42:42 +0300

    stash test2

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from databases import Database
-from src.core.settings import get_settings
+from databases import DatabaseURL
 from src.api import include_routers
 from src.core.settings import get_settings
 import src.controller.todo as todo_module
